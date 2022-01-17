fd7811fab0e4d9afba8cb47e8e39b780d3944eab
charleskessler <6663095+charleskessler@users.noreply.github.com>
2022-01-17 14:27:01 -0500

    Add Artist Module

---
+++ Dotify.Monolith/src/Dotify.Api/Features/Artists/Modules/ArtistModule.cs
@@ -0,0 +1,15 @@
+﻿using Carter;
+using Carter.Response;
+
+namespace Dotify.Api.Features.Artists.Modules;
+
+public class ArtistModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/artists/{id}", (string id, HttpResponse res) =>
+        {
+            return res.Negotiate("Hi!");
+        });
+    }
+}
