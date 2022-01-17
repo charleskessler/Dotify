bfc7bbec15bfa4f41c93c243ccbd85dd489c7395
charleskessler <6663095+charleskessler@users.noreply.github.com>
2022-01-17 14:13:01 -0500

    Add Carter

---
+++ Dotify.Monolith/src/Dotify.Api/Startup/CarterRegistration.cs
@@ -0,0 +1,31 @@
+﻿using Carter;
+
+namespace Dotify.Api.Startup
+{
+    public static class CarterRegistration
+    {
+        public static IServiceCollection AddCarterInt(this IServiceCollection services, string swaggerTitle = "Swagger")
+        {
+            services.AddCarter();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCarter(this IApplicationBuilder app, bool withSwagger = true, string swaggerTitle = "Swagger")
+        {
+            app.UseRouting();
+            app.UseEndpoints(builder => builder.MapCarter());
+            // Configure the HTTP request pipeline.
+            if (withSwagger)
+            {
+                app.UseSwaggerUI(opt =>
+                {
+                    opt.RoutePrefix = "openapi/ui";
+                    opt.SwaggerEndpoint("/openapi", swaggerTitle);
+                });
+            }
+
+            return app;
+        }
+    }
+}
