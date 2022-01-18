51db6c9646ccde3cc84fa8e2f699101007d3c8a4
charleskessler <6663095+charleskessler@users.noreply.github.com>
2022-01-18 11:42:29 -0500

    Expand custom schema IDs to be able to use DisplayNameAttribute in addition to Dto replacement

--- Dotify.Monolith/src/Dotify.Api/Program.cs
+++ Dotify.Monolith/src/Dotify.Api/Program.cs
@@ -6,9 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFeatures();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => 
+builder.Services.AddSwaggerGen(options =>
 {
-    options.CustomSchemaIds(x => x.Name.EndsWith("Dto") ? x.Name.Replace("Dto", string.Empty) : x.Name);
+    options.CustomSchemaIds(x => x.GetCustomAttributes(false).OfType<DisplayNameAttribute>().FirstOrDefault()?.DisplayName
+                                  ?? (x.Name.EndsWith("Dto") ? x.Name.Replace("Dto", string.Empty) : x.Name));
 
     options.DocInclusionPredicate((s, description) =>
     {
